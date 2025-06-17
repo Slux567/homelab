f5338502e4e4225bd5cd4dbd6464ca8f04003747
slux567 <stijnluijkx@gmail.com>
2025-06-17 21:22:38 +0200

    NEW - Ansible tasks for updating all machines and role for Odin with first tasks to install docker

---
+++ ansible/roles/odin/README.md
@@ -0,0 +1,20 @@
+odin
+=========
+This role installs all the basic stuff on the odin node
+
+Requirements
+------------
+
+EXAMPLE
+- a machine running Ubuntu
+- blablabla
+
+
+
+
+
+
+
+
+
+
---
+++ terraform/odin/variables.tf
@@ -0,0 +1,20 @@
+variable "proxmox_api_url" {
+  type = string
+}
+variable "proxmox_token_id" {
+  type = string
+}
+variable "proxmox_token_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "public_ssh_key" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "vm_user" {
+  type      = string
+}
