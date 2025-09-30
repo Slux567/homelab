665c133e16b13fdbc2d44b39a3617ea646dafa6c
slux567 <stijnluijkx@gmail.com>
2025-09-30 23:35:17 +0200

    NEW - nfs-subdir-provisioner deploy + values which makes it possible to automatically create subdirs in NFS datasets, so that I dont need to create a dataset per application

---
+++ kubernetes/storage/nfs-subdir-provisioner/deploy.sh
@@ -0,0 +1,4 @@
+kubectl create namespace nfs-storage
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm repo update
+helm install nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner -n nfs-storage -f values.yaml
